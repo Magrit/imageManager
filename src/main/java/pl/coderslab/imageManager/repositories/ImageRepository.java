a8f24a694c5bc23c18e00115af1983abaa5a0d47
Gosia Jedrzejewska <gorzata.jedrzejewska@gmail.com>
2021-03-22 17:15:49 +0100

    image package

--- src/main/java/pl/coderslab/imageManager/repositories/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.coderslab.imageManager.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.coderslab.imageManager.model.ImageDescription;
-
-public interface ImageRepository extends JpaRepository<ImageDescription, Long> {
-
-}
